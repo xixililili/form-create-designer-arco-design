d291ced377223b48c0baae8a9970052c1dc10a7a
汪杰 <312976070@qq.com>
2024-02-28 17:36:59 +0800

    #

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,4 @@
 import {createApp} from 'vue';
-import ELEMENT from 'element-plus';
-import 'element-plus/dist/index.css';
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
 import formCreate from '@form-create/arco-design';
@@ -9,7 +7,6 @@ import FcDesigner from '../src/index';
 
 const app = createApp(App);
 
-app.use(ELEMENT);
 app.use(ArcoVue);
 app.use(formCreate);
 app.use(FcDesigner);
